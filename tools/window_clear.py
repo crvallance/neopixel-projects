472391675d8df2ab4c4123d9617c9ed5f6cd094f
crvallance <crvallance@gmail.com>
2019-12-16 03:55:48 +0000

    simple script to clear all pixels

---
+++ tools/window_clear.py
@@ -0,0 +1,14 @@
+fo = open("/dev/rpmsg_pru30", "w")
+
+def clear_all():
+    for i in range(0, 364):
+        # print(i)
+        fo.write("%d %d %d %d\n" % (i, 0, 0, 0))
+        fo.flush()
+        # sleep(.5)
+    fo.write("-1 0 0 0\n")
+    fo.flush()
+
+if __name__ == "__main__":
+    clear_all()
+    fo.close()
