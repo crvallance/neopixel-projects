bddc74f1ee4fce67955e274205b9cc743baf922f
crvallance <crvallance@gmail.com>
2021-10-30 17:42:55 +0000

    abstracted write and other junk

--- pixel_worker.py
+++ pixel_worker.py
@@ -19,23 +19,17 @@ class Pattern(object):
         self.fo.write(f'{pixel_location} {r} {g} {b}\n')
         self.fo.flush()
         if self.push:
-            # self.fo.write('-1 0 0 0\n')
-            # self.fo.flush()
             self.commit()
 
     def paint_all_windows(self, color):
         for pixel in range(0, self.pixel_count):
             self.write(pixel_location=pixel, color=color)
-        # self.fo.write('-1 0 0 0\n')
-        # self.fo.flush()
         self.commit()
-  
+
     def paint_pixel_list(self, color, pixels: list, push: bool = False):
         for pixel in pixels:
             self.write(pixel_location=pixel, color=color)
         if push:
-            # self.fo.write('-1 0 0 0\n')
-            # self.fo.flush()
             self.commit()
 
 
@@ -44,16 +38,17 @@ class Strobe(Pattern):
         self.__color = color
         super().__init__()
 
-    def run(self, color, wait: int = .125):
+    def run(self, color):
         self.paint_all_windows(Colors.off)
         self.paint_all_windows(self.__color)
         self.paint_all_windows(Colors.off)
         self.paint_all_windows(self.__color)
         self.paint_all_windows(Colors.off)
 
-    def loop(self, color: tuple = (255, 255, 255), loop_count: int = 20, wait: int = 1):
+    def loop(self, color: tuple = (255, 255, 255), loop_count: int = 20, wait: int = 0):
         for loop in range(0, loop_count):
-            self.run(color, wait)
+            self.run(color)
+            time.sleep(wait)
 
 
 class Marquee(Pattern):
@@ -83,36 +78,56 @@ class Marquee(Pattern):
             self.run(color, wait)
 
 
+class SimpleChase(Pattern):
+    def __init__(self, color: tuple = (255, 255, 255)):
+        self.__color = color
+        super().__init__()
+
+    def run(self, color, wait: int = 1):
+        self.paint_all_windows(Colors.off)
+        self.push = True
+        for i in range(0, self.pixel_count):
+            self.write(color, pixel_location=i)
+            # self.paint_positions([i], color, push=True)
+            time.sleep(wait)
+        for i in range(0, self.pixel_count):
+            self.write(pixel_location=i, color=Color.off)
+            #self.paint_positions([i], self.__off, push=True)
+            time.sleep(wait)
+
+
 @dataclass
 class Colors:
     off: tuple = (0, 0, 0)
-    # aqua: tuple = (0, 0x10, 0x10)
+    aqua: tuple = (0, 0x10, 0x10)
     black: tuple = (6.02344, 6.02344, 6.02344)
-    # blue1: tuple = (0, 0, 0x10)
+    blue1: tuple = (0, 0, 0x10)
     blue2: tuple = (0, 0, 255)
-    # green: tuple = (0, 0x10, 0)
+    green: tuple = (0, 0x10, 0)
     halloween_orange: tuple = (164.64062, 38.14844, 2.00781)
     jen_orange: tuple = (164.64062, 38.14844, 2.00781)
     jen_red: tuple = (74.2890625, 0.0, 0.0)
     orange: tuple = (245, 25, 0)
     pink: tuple = (200, 25, 31)
-    # purple1: tuple = (0x10, 0, 0x10)
+    purple1: tuple = (0x10, 0, 0x10)
     purple2: tuple = (36.14062, 4.01562, 51.19922)
-    # red1: tuple = (0x10, 0, 0)
-    # red2: tuple = 0x100000  (0x10, 0, 0)
+    red1: tuple = (0x10, 0, 0)
+    red2: tuple = (0x10, 0, 0)
     red3: tuple = (74.2890625, 0.0, 0.0)
-    # yellow: tuple = (0x10, 0x10, 0)
+    yellow: tuple = (0x10, 0x10, 0)
 
 
 def main():
-    # meh = Strobe(Colors.blue2)
+    meh = Strobe()
+    meh.loop(color=Colors.blue2)
     # for loop in range(0, meh.loops):
         # meh.run()
-    # hi = Marquee()
-    hi = Strobe()
+    # hi = SimpleChase()
+    # hi = Marquee(Colors.blue2)
+    # hi = Strobe()
     # for loop in range(0, hi.loops):
     #     hi.run(color=Colors.halloween_orange)
-    hi.loop(color=Colors.halloween_orange)
+    # hi.loop(color=Colors.blue1)
 
 
 '''
@@ -122,7 +137,7 @@ class ALLPattern(object):
         self.__wait: int = wait
         self.__colors: list = []
         self.__off = (0, 0, 0)
-        self.pixnum: int = 364
+        self.pixel_count: int = 364
         self.right_window = range(0, 120)
         self.center_window = range(120, 244)
         self.left_window = range(244, 364)
@@ -139,51 +154,15 @@ class ALLPattern(object):
         self.__colors.append(color)
         return self
 
-    def paint_single_color(self, color: tuple):
-        r, g, b = color
-        for i in self.all_windows:
-            self.fo.write("%d %d %d %d\n" % (i, r, g, b))
-            self.fo.flush()
-        self.fo.write("-1 0 0 0\n")
-
-    def paint_positions(self, window, color: tuple, push=False):
-        r, g, b = color
-        for item in window:
-            self.fo.write("%d %d %d %d\n" % (item, r, g, b))
-            self.fo.flush()
-        if push:
-            self.fo.write("-1 0 0 0\n")
-            self.fo.flush()
-
-    def marquee(self, color: tuple):
-        if not color:
-            color = (0, 0, 0x10)
-        evens = []
-        odds = []
-        for num in range(self.pixnum):
-            if num % 2 == 0:
-                evens.append(num)
-            else:
-                odds.append(num)
-        self.paintPositions(evens, color)
-        self.paintPositions(odds, self.__off)
-        self.fo.write("-1 0 0 0\n")
-        self.fo.flush()
-        time.sleep(self.wait)
-        self.paintPositions(evens, self.__off)
-        self.paintPositions(odds, color)
-        self.fo.write("-1 0 0 0\n")
-        self.fo.flush()
-        time.sleep(self.wait)
 
     def simple_chase(self, color: tuple):
         self.paint_single_color(self.__off)
         if not color:
             color = (0, 0, 0x10)
-        for i in range(self.pixnum):
+        for i in range(self.pixel_count):
             self.paint_positions([i], color, push=True)
             time.sleep(self.wait)
-        for i in range(self.pixnum):
+        for i in range(self.pixel_count):
             self.paint_positions([i], self.__off, push=True)
             time.sleep(self.wait)
 
@@ -222,10 +201,10 @@ class ALLPattern(object):
     def popcorn(self, color: tuple):
         self.paint_single_color(self.__off)
         percent = 20
-        all_pixels = list(range(0, self.pixnum))
+        all_pixels = list(range(0, self.pixel_count))
         dim_white = (36.140625, 36.140625, 36.140625)
         sleepz = [.25, .5, .75, .125, .0625, 1, 1.5]
-        while len(all_pixels) > int(self.pixnum * percent / 100):
+        while len(all_pixels) > int(self.pixel_count * percent / 100):
             pixel = all_pixels.pop(random.randrange(len(all_pixels)))
             time.sleep(random.choice(sleepz))
             print(pixel)
