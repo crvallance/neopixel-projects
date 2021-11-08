e5dba1b0408e8d3bf5b4e4a8426309f6e68255c0
crvallance <crvallance@protonmail.com>
2021-11-07 21:51:53 -0600

    setup pattern as fixture (is this ok?)

--- tests/pixel_worker_test.py
+++ tests/pixel_worker_test.py
@@ -7,17 +7,22 @@ def generic_file(tmp_path_factory):
     file_name = tmp_path_factory.mktemp("data") / "dummyfile"
     return file_name
 
-
-def test_pattern_write(generic_file):
+@pytest.fixture()
+def pattern_instance(generic_file):
     pattern = pixel_worker.Pattern(pru=generic_file)
-    pattern.write(pixel_location=0, color=(0, 0, 0))
+    return pattern
+
+def test_pattern_write(generic_file, pattern_instance):
+    # pattern = pixel_worker.Pattern(pru=generic_file)
+    # pattern = pattern_instance()
+    pattern_instance.write(pixel_location=0, color=(0, 0, 0))
     f = open(generic_file, "r")
     lines = f.readlines()
     assert lines == ['0 0 0 0\n']
 
-def test_pattern_commit(generic_file):
-    pattern = pixel_worker.Pattern(pru=generic_file)
-    pattern.commit()
+def test_pattern_commit(generic_file, pattern_instance):
+    # pattern = pixel_worker.Pattern(pru=generic_file)
+    pattern_instance.commit()
     f = open(generic_file, "r")
     lines = f.readlines()
     assert lines == ['-1 0 0 0\n']
