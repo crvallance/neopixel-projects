e76b60f2e877c597581b658e17495592f29732f1
crvallance <crvallance@protonmail.com>
2021-11-09 20:23:56 -0600

    continuing tests

--- tests/pixel_worker_test.py
+++ tests/pixel_worker_test.py
@@ -15,8 +15,6 @@ def pattern_instance(generic_file):
 
 
 def test_pattern_write(generic_file, pattern_instance):
-    # pattern = pixel_worker.Pattern(pru=generic_file)
-    # pattern = pattern_instance()
     pattern_instance.write(pixel_location=0, color=(0, 0, 0))
     f = open(generic_file, "r")
     lines = f.readlines()
@@ -24,7 +22,6 @@ def test_pattern_write(generic_file, pattern_instance):
 
 
 def test_pattern_commit(generic_file, pattern_instance):
-    # pattern = pixel_worker.Pattern(pru=generic_file)
     pattern_instance.commit()
     f = open(generic_file, "r")
     lines = f.readlines()
@@ -44,3 +41,28 @@ def test_pattern_paint_all_windows(generic_file, push_param):
         assert lines == ['0 0 0 0\n', '-1 0 0 0\n', '1 0 0 0\n', '-1 0 0 0\n', '-1 0 0 0\n']
     else:
         assert lines == ['0 0 0 0\n', '1 0 0 0\n', '-1 0 0 0\n']
+
+
+def test_strobe(generic_file):
+    # mypattern = pixel_worker.Pattern()
+    strobe = pixel_worker.Strobe().with_pru(generic_file)
+    strobe.run(color=(0, 0, 0))
+    f = open(generic_file, "r")
+    lines = f.readlines()
+
+# class Strobe(Pattern):
+#     def __init__(self, color: tuple = (255, 255, 255)):
+#         self.__color = color
+#         super().__init__()
+
+#     def run(self, color):
+#         self.paint_all_windows(Colors.off)
+#         self.paint_all_windows(self.__color)
+#         self.paint_all_windows(Colors.off)
+#         self.paint_all_windows(self.__color)
+#         self.paint_all_windows(Colors.off)
+
+#     def loop(self, color: tuple = (255, 255, 255), loop_count: int = 20, wait: int = 0):
+#         for loop in range(0, loop_count):
+#             self.run(color)
+#             time.sleep(wait)
